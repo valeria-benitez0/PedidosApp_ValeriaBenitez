ef9eca56e69ca45522ed7e6fd4ea2c00588965e9
valeria.benitez889@pascualbravo.edu.co <valeria.benitez889@pascaulbravo.edu.co>
2025-04-28 21:57:35 -0500

    Aplicar patrón Singleton con RegistroPedidos

---
+++ RegistroPedidos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PedidosApp
+{
+    public sealed class RegistroPedidos
+    {
+        private static RegistroPedidos _instancia;
+        private static readonly object _lock = new object();
+        public List<Pedido> Pedidos { get; private set; }
+
+        private RegistroPedidos()
+        {
+            Pedidos = new List<Pedido>();
+        }
+
+        public static RegistroPedidos Instancia
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (_instancia == null)
+                        _instancia = new RegistroPedidos();
+                    return _instancia;
+                }
+            }
+        }
+
+        public void AgregarPedido(Pedido pedido)
+        {
+            Pedidos.Add(pedido);
+        }
+    }
+}
